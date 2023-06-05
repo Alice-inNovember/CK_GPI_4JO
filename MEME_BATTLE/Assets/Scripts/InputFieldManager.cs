8e86c81feca8ceff004e766e0e40b82864de0198
junlee2 <junlee2@student.42seoul.kr>
2023-06-05 14:52:40 +0900

    add : input System

--- MEME_BATTLE/Assets/Scripts/InputFieldManager.cs
+++ MEME_BATTLE/Assets/Scripts/InputFieldManager.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -6,25 +7,5 @@ using UnityEngine.UI;
 
 public class InputFieldManager : MonoBehaviour
 {
-    [SerializeField] private GameObject inputFieldOBJ;
-    
-    private TMP_InputField _inputField;
-    
-    
-    // Start is called before the first frame update
-    void Start()
-    {
-        _inputField = inputFieldOBJ.GetComponent<TMP_InputField>();
-    }
-
-    public void OnSelect()
-    {
-        _inputField.text = "";
-    }
-    
-    public void OnEdit()
-    {
-        if(_inputField.text.Length > 1)
-            _inputField.text = "";
-    }
+	
 }
