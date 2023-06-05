14e98f1652834a68ffa72897240cf0a592017d4f
IM TAE HUI <werey1537@gmail.com>
2023-06-05 14:51:41 +0900

    Singleton 도입

--- MEME_BATTLE/Assets/Scripts/Singleton.cs
+++ MEME_BATTLE/Assets/Scripts/Singleton.cs
@@ -1,5 +1,39 @@
-public class Singleton<T> where T : class, new()
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Singleton<T> : MonoBehaviour where T : Singleton<T>
 {
-    static T _instance = null;
-    public static T Instance => _instance ??= new T();
+    private static T instance = null;
+    protected bool dontDestroyOnLoad = false;
+
+    protected virtual void Awake()
+    {
+        if (instance == null)
+        {
+            instance = (T)this;
+
+            if (dontDestroyOnLoad)
+            {
+                DontDestroyOnLoad(this.gameObject);
+            }
+        }
+        else
+        {
+            Destroy(this.gameObject);
+        }
+    }
+
+    public static T Instance
+    {
+        get
+        {
+            if (instance == null)
+            {
+                return null;
+            }
+
+            return instance;
+        }
+    }
 }
